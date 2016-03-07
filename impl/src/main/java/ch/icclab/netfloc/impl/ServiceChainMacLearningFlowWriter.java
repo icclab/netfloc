af9b0c6641ddfa0a6ffaf7ad3235f35e5cbb9837
Denis Gerold Baudinot <badi@zhaw.ch>
2016-03-07 10:36:42 +0100

    fixes

--- impl/src/main/java/ch/icclab/netfloc/impl/ServiceChainMacLearningFlowWriter.java
+++ impl/src/main/java/ch/icclab/netfloc/impl/ServiceChainMacLearningFlowWriter.java
@@ -169,7 +169,7 @@ public class ServiceChainMacLearningFlowWriter implements IMacLearningListener {
 		flowBuilder.setMatch(matchBuilder.build());
 
 		// TODO generate flow id
-		String flowId = "ServiceChainRewrite_" + chainId + "_" connId + "_" + 0 + "_" + beginBridge.getDatapathId();
+		String flowId = "ServiceChainRewrite_" + chainId + "_" + connId + "_" + 0 + "_" + beginBridge.getDatapathId();
 		flowBuilder.setId(new FlowId(flowId));
 		FlowKey key = new FlowKey(new FlowId(flowId));
 
